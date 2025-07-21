84a31b86c13ecdcee95e533e22d02cae12c723c4
Ahadul Haque Shovo <shovo2522@gmail.com>
2025-07-21 23:37:25 +0600

    Create scatter.py

---
+++ scatter.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x=np.array([0,1,2,6,8,9])
+y=np.array([0,1,3,5,8,10])
+
+plt.scatter(x,y,color='red')
+
+
+#day two
+x=np.array([0,3,4,5,8,7])
+y=np.array([0,4,3,7,8,10])
+plt.scatter(x,y,color='orange')
+plt.show()
